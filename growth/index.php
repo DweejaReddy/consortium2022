a80e06ddb471c2c16bcee752ffbfc780309f9858
asachkahitari <patildeep2000@gmail.com>
2022-03-03 18:31:20 +0530

    growth added

---
+++ growth/index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Growth Hacking Internship | Consortium '22, E-CELL, VNIT, 2022</title>
+</head>
+<?php include('includes/header.php') ?>
+<link rel="stylesheet" href="css/eventStyle.css">
+
+<body>
+  <hr style="border-top: 1px solid #fff;opacity: 0.8;margin-top: 3px;" class="mt-2">
+  <section class="d-none d-md-block my-5" id="home">
+    <div class="about row align-items-center py-4 mx-auto" style="max-width: 80%;">
+
+
+      <div class="col-12 col-md-5 text-white ml-4 pt-4">
+        <h1 style="font-weight: 900;text-shadow: 0px 1px, 1px 0px, 1px 1px;font-size: 3.5rem;" data-aos="fade-down" data-aos-duration="2000">Growth Hacking Internship</h1>
+        <!-- <h5  style="font-weight: 500;text-shadow: 0px 1px, 1px 0px, 1px 1px;font-size: 1.3rem;" data-aos="fade-right" data-aos-duration="2000">UNCOVER THE FUTURE LEADERS</h5> -->
+        <p style="color: #D5242A;font-size: 1.4vw;">Get a chance to add a star attraction to your Resume in your first year itself and get ahead of the pack.
+        </p>
+        <!-- <p style="font-size: 1.2vw;" class="my-4">In this era of hyper-competition and non-stop disruption, the only way to stand out from the crowd is to stand for something unique. CEO is more than a competition; it’s a mission to decode future leaders; it’s a chance for students to show, test, and improve their leadership skills in a real-world setting. An event that provides a glimpse into the future, where excellent leadership is necessary for success, and where participants take risks and even thrive during the most challenging times. -->
+        </p>
+        <a href="https://forms.gle/dhkmw4XBfxNdKUZb8"><button class="btn btn3 px-3" style="font-size: 1.4vw;">Apply Now for Free!!</button></a>
+        <!-- <a href="https://drive.google.com/file/d/16gdF-f04rbMDK5ogoHDiRiUc4BrlMtTS/view?usp=sharing" target="_blank"><button class="btn btn3 ml-2 my-2" style="font-size: 1.4vw;" >Brochure</button></a><br> -->
+        <p style="color: rgb(160, 160, 160);font-size: 1.3vw;" class="ml-5 mt-2">Last Date: 13th March</p>
+      </div>
+
+      <div class="col-12 col-md-5">
+        <div class="owl-carousel owl-theme pt-5 owl-loaded owl-drag" id="one">
+
+          <div class="owl-stage-outer">
+            <div class="owl-stage" style="transform: translate3d(-932px, 0px, 0px); transition: all 0s ease 0s; width: 2330px;">
+              <div class="owl-item active" style="width: 456px; margin-right: 10px;">
+                <div class="item">
+                  <img src="img/GHI.jpeg" alt="">
+                </div>
+              </div>
+              <div class="owl-item cloned" style="width: 456px; margin-right: 10px;">
+                <div class="item">
+                  <img src="img/GHI1.jpeg" alt="">
+                </div>
+              </div>
+              <div class="owl-item cloned" style="width: 456px; margin-right: 10px;">
+                <div class="item">
+                  <img src="img/GHI2.jpeg" alt="">
+                </div>
+              </div>
+              <div class="owl-item cloned" style="width: 456px; margin-right: 10px;">
+                <div class="item">
+                  <img src="img/GHI3.jpeg" alt="">
+                </div>
+              </div>
+              <div class="owl-item cloned" style="width: 456px; margin-right: 10px;">
+                <div class="item">
+                  <img src="img/GHI4.jpeg" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="owl-nav disabled"><button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button></div>
+          <div class="owl-dots disabled"><button role="button" class="owl-dot active"><span></span></button></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="d-block d-md-none my-5" id="home_mob">
+    <div class="about row align-items-center py-4 mx-auto d-block d-lg-none">
+      <h1 class="text-center text-white mb-4" style="font-weight: 900;font-size: 3rem;">Growth Hacking Internship</h1>
+      <div class="col-12 col-md-5">
+        <div class="owl-carousel owl-theme" id="two">
+          <div class="item">
+            <img src="img/GHI poster final.png" alt="">
+          </div>
+        </div>
+      </div>
+
+      <div class="col-12 col-md-5 text-white text-center pt-3">
+        <!-- <h6  style="font-weight: 500;text-shadow: 0px 1px, 1px 0px, 1px 1px;font-size: 4.5vw;" class="mt-4">UNCOVER THE FUTURE LEADERS</h6> -->
+        <p style="color: #D5242A;font-size: 3.7vw;">Get a chance to add a star attraction to your Resume in your first year itself and get ahead of the pack.
+
+        </p>
+        <!-- <p style="color: rgb(160, 160, 160);font-size: 3.7vw;" class="text-center my-3" >In this era of hyper-competition and non-stop disruption, the only way to stand out from the crowd is to stand for something unique. CEO is more than a competition; it’s a mission to decode future leaders; it’s a chance for students to show, test, and improve their leadership skills in a real-world setting. An event that provides a glimpse into the future, where excellent leadership is necessary for success, and where participants take risks and even thrive during the most challenging times. -->
+
+        </p>
+        <div class="text-center mt-3 px-5">
+          <a href="https://forms.gle/tq5ciVbSLdRduxvH6" target="_blank"><button type="button" class="btn btn3 my-2 px-3" style="font-size: 3.7vw;">Apply Now for Free!!</button></a>
+          <!-- <a href="https://drive.google.com/file/d/16gdF-f04rbMDK5ogoHDiRiUc4BrlMtTS/view?usp=sharing" target="_blank"><button type="button" class="btn btn3 my-2 px-5" style="font-size: 3.7vw;">Brochure</button></a> -->
+          <p style="color: rgb(160, 160, 160);font-size: 4.2vw;" class=" mt-2">Last Date: 13th March</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+  <!-- <section class="my-5 text-center " id="struct">
+        <h1  style="font-weight: 900;font-size: 2.3rem;" class="text-white">EVENT STRUCTURE</h1>
+        <div class="row align-items-center">
+            <div class="col-1">
+                <div class="ceoE"><div class="ceoline1"></div><div class="ceoline2"></div></div>
+            </div>
+            <div class="col-7">
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">QUALIFYING ROUND </h4>
+                    <p class="text-left ml-3">The qualifying round begins the moment you register, with the completion of a basic questionnaire covering corporate and financial topics. Keep in mind that you must submit your responses before we close registrations.
+Our experts will shortlist the contenders for the first round based on their critical thinking, decisiveness, business-related strategizing, and creative thinking.
+                    
+            </div>
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">ROUND 1: GROUP DISCUSSION</h4>
+                    <br>
+                  <p class="text-left ml-3">Industry experts will conduct a well-curated webinar for all participants, walking you through the best way to ace a group discussion. Then, in our group discussion round, we'll put your learnings and communication abilities to the test. The best performances in each group will progress to the next round.	                  
+            </div>
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">ROUND 2: MARKETISTAAN</h4>
+                    <br>
+                  <p class="text-left ml-3">Marketistaan is a market analysis case study round where participants qualifying the first round will then be divided into random teams to test the flexibility of the work environment. Different teams will compete against one another in providing the most practical corporate solution to the case study offered.
+                </p>
+                  
+            </div>
+            <div class="struct my-4 py-4" >
+                <h4 class="text-left ml-3">ROUND 3: THE BOARDROOM DILEMMA</h4>
+                    <br>
+                  <p class="text-left ml-3">Real problems require real answers, putting your analytical, problem-solving, and convincing abilities to the test. Participants in this round will showcase what it takes to be on the board of directors as well as to be the executive directors of a company that is facing a crucial decision. Make allies, understand your opponents, influence the neutral people, and present your point of view to the corporation's other stakeholders.
+                </p>
+                  
+            </div>
+        </div>
+        </div>
+        <span><img src="img/wireframe.png" alt="" width="1300px" style="position: relative;bottom: 1250px;z-index:-1;margin-bottom: -1280px;left: -10px;opacity: 0.7;"></span>
+    </section> -->
+  <section>
+    <div class="text-center about mx-auto my-5 py-5" style="max-width: 80%;">
+      <h1 style="font-weight: 900;font-size: 2.3rem;color: #D5242A;">WHAT WILL YOU LEARN</h1>
+      <section>
+        <div class="text-center about mx-auto my-5 py-5" style="max-width: 75%; background: #fff;">
+          <h2 style="font-weight: 900;font-size: 2.0rem;color: #D5242A;">Growth Hacking</h2>
+          <div id="grid" class="grid-container">
+            <div class="grid-item" style="font-weight: 600;font-size: 1.0rem;color: #3a3a3a; padding:10px"><img width="100px" src="img/gmail.png"><br><br>hacks to get anyone’s Email id</div>
+            <div class="grid-item" style="font-weight: 600;font-size: 1.0rem;color: #3a3a3a; padding:10px"><img width="100px" src="img/man.png"><br><br>Secrets of Linkedin profile optimisation</div>
+            <div class="grid-item" style="font-weight: 600;font-size: 1.0rem;color: #3a3a3a; padding:10px"><img width="100px" src="img/networking.png"><br><br>Blueprint of Linkedin Network Booster</div>
+            <div class="grid-item" style="font-weight: 600;font-size: 1.0rem;color: #3a3a3a; padding:10px"><img width="100px" src="img/analysis.png"><br><br>Frameworks for Competitors analysis</div>
+            <div class="grid-item" style="font-weight: 600;font-size: 1.0rem;color: #3a3a3a; padding:10px"><img width="100px" src="img/linkedin-premium.png"><br><br>How to get Linkedin Premium for FREE?</div>
+            <div class="grid-item" style="font-weight: 600;font-size: 1.0rem;color: #3a3a3a; padding:10px"><img width="100px" src="img/settings.png"><br><br>MindBlowing Growth Hacking tools and many more...... </div>
+          </div>
+        </div>
+      </section>
+      <style>
+        #grid {
+          display: grid;
+          grid-template-columns: auto auto auto;
+          padding: 40px;
+        }
+
+        @media all and (max-width: 1240px) {
+          #grid {
+            display: grid;
+            grid-template-columns: auto;
+          }
+        }
+      </style>
+      <section>
+        <div class="text-center about mx-auto my-5 py-5" style="max-width: 75%; background: #fff;">
+          <h2 style="font-weight: 900;font-size: 2.0rem;color: #D5242A;">Brand Development</h2>
+          <div style="font-weight: 500;font-size: 1.5rem;color: #3a3a3a; padding:10px"> Popularising information about E-CELL’s events in your college through various and diverse platforms.
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="text-center about mx-auto my-5 py-5" style="max-width: 75%; background: #fff;">
+          <h2 style="font-weight: 900;font-size: 2.0rem;color: #D5242A;">Organize</h2>
+          <div style="font-weight: 500;font-size: 1.5rem;color: #3a3a3a; padding:10px">Organize entrepreneurship-related events in your college in coordination with E-Cell VNIT.
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="text-center about mx-auto my-5 py-5" style="max-width: 75%; background: #fff;">
+          <h2 style="font-weight: 900;font-size: 2.0rem;color: #D5242A;">Social Media</h2>
+          <div style="font-weight: 500;font-size: 1.5rem;color: #3a3a3a; padding:10px"> E-posters on social media platforms like Facebook, Instagram and WhatsApp. Be the face of E-Cell VNIT in your college.
+          </div>
+        </div>
+      </section>
+    </div>
+  </section>
+
+  <section>
+    <div class="text-center about mx-auto my-5 py-5" style="max-width: 80%;">
+      <h1 style="font-weight: 900;font-size: 2.3rem;color: #D5242A;">PERKS FOR EVERYONE</h1>
+      <ul class="text-center reason mx-auto">
+        <li class="py-2 container">
+          Certificates of National Importance.
+        </li>
+        <li class="py-2 container">
+          Paid Summer Internship Opportunities.
+        </li>
+        <li class="py-2 container">
+          “Stock Market made easy” Course worth Rs 1000 from E-Learn Market
+        </li>
+        <li class="py-2 container">
+          Access to Cheatsheet of Growth Hacking tools worth Rs 5000 each
+        </li>
+        <li class="py-2 container">
+          Free Zerodha Demat account worth Rs 200 each
+        </li>
+        <li class="py-2 container">
+          Stipend based on your performance.
+        </li>
+        <li class="py-2 container">
+          Free Premium Subscriptions, Vouchers and goodies for all.
+        </li>
+        <li class="py-2 container">
+          Courses for Personal Growth.
+        </li>
+        <li class="py-2 container">
+          Access to our Community
+        </li>
+        <li class="py-2 container">
+          Offer Letter to boost your Resume.
+        </li>
+      </ul>
+    </div>
+  </section>
+  <section>
+    <div id="perks" class="text-center about mx-auto my-5 py-5" style="max-width: 80%;">
+      <h1 style="font-weight: 900;font-size: 2.3rem;color: #D5242A;">PERKS FOR TOP PERFORMERS</h1>
+      <ul class="text-center reason mx-auto">
+        <li class="py-2 container">
+          Basics of Stock Market Course worth Rs 5000 each
+        </li>
+        <li class="py-2 container">
+          Letter of Recommendation
+        </li>
+        <li class="py-2 container">
+          Linkedin endorsements
+        </li>
+        <li class="py-2 container">
+          Free access to E-Cell VNIT’s Consortium paid events.
+        </li>
+        <li class="py-2 container">
+          Personalised Training
+        </li>
+        <li class="py-2 container">
+          E-Cell Merchandise Worth Rs 400 each
+        </li>
+        <li class="py-2 container">
+          Weekly shoutout
+        </li>
+        <li class="py-2 container">
+          Special Goodies </li>
+      </ul>
+    </div>
+
+  </section>
+
+  <section>
+    <div class="text-center about cont text-white mx-auto my-5 py-5" style="max-width: 80%;" id="managers">
+      <h1 style="font-weight: 900;font-size: 2.3rem;color: #D5242A;">Contact Us</h1>
+      <p class="px-3 my-4">For any event related queries, do reach out to us</p>
+      <div class="row align-items-center">
+        <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+          <h5 class="mt-2 pt-3">Harsh Yadav</h5>
+          <a class="ml-4" href="https://www.linkedin.com/in/harshy01/" target="_blank">
+            <img id="linkedin" src="img/linkedin_black.png" alt="">
+          </a>
+          <!-- <div class="name">bhargav@gmail.com</div> -->
+          <div class="phone pb-4" style="margin-top: -30px;">+91 84589 67669</div>
+        </div>
+        <!-- <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+                    <h5 class="mt-2 pt-3">Sourabh Bedkute</h5>
+                        <img id="linkedin" src="img/linkedin_black.png" alt="">
+                    <div class="phone pb-4" style="margin-top: -30px;">+91 91563 77253</div>
+                </div> -->
+        <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+          <h5 class="mt-2 pt-3">Alisha Hatalkar</h5>
+          <a class="ml-4" href="https://www.linkedin.com/in/alisha-amit-hatalkar" target="_blank">
+            <img id="linkedin" src="img/linkedin_black.png" alt="">
+          </a>
+          <div class="phone pb-4" style="margin-top: -30px;">+91 98208 16092</div>
+        </div>
+        <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+          <h5 class="mt-2 pt-3">Bhushan Bhale</h5>
+          <a class="ml-4" href="https://www.linkedin.com/in/bhushan-bahale/" target="_blank">
+            <img id="linkedin" src="img/linkedin_black.png" alt="">
+          </a>
+          <div class="phone pb-4" style="margin-top: -30px;">+91 96106 57385</div>
+        </div>
+        <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+          <h5 class="mt-2 pt-3">Ashish Nagori</h5>
+          <a class="ml-4" href="https://www.linkedin.com/in/ashish-nagori-3306b520a" target="_blank">
+            <img id="linkedin" src="img/linkedin_black.png" alt="">
+          </a>
+          <div class="phone pb-4" style="margin-top: -30px;">+91 77698 79851</div>
+        </div>
+        <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+          <h5 class="mt-2 pt-3">Mitali Khoke</h5>
+          <a class="ml-4" href="https://www.linkedin.com/in/mitali-khodke-63408220b/" target="_blank">
+            <img id="linkedin" src="img/linkedin_black.png" alt="">
+          </a>
+          <div class="phone pb-4" style="margin-top: -30px;">+91 93095 76732</div>
+        </div>
+        <div class="contactcard col-12 col-md-6 col-lg-4 mx-4" style="height: auto;border-radius: 25px;" data-aos="fade-up" data-aos-duration="2000">
+          <h5 class="mt-2 pt-3">Kushagra Upadhyaya</h5>
+          <a class="ml-4" href="https://www.linkedin.com/in/kushagra-upadhyaya-967b7220a" target="_blank">
+            <img id="linkedin" src="img/linkedin_black.png" alt="">
+          </a>
+          <div class="phone pb-4" style="margin-top: -30px;">+91 94147 41300</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php include('includes/footer.php') ?>
+</body>
+
+</html>
